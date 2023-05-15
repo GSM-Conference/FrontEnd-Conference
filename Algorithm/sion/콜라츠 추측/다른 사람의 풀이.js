f7926d01a7e850dfbe189385d5a376d49e9f56a4
yoosion030 <yoosioff@gmail.com>
2023-05-16 08:18:48 +0900

    콜라츠 추측

---
+++ Algorithm/sion/콜라츠 추측/다른 사람의 풀이.js
@@ -0,0 +1,8 @@
+function collatz(num) {
+  var answer = 0;
+  while (num != 1 && answer != 500) {
+    num % 2 == 0 ? (num = num / 2) : (num = num * 3 + 1);
+    answer++;
+  }
+  return num == 1 ? answer : -1;
+}
