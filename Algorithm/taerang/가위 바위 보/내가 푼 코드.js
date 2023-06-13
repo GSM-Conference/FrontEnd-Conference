493dd3ff4e1f6eb445f147a206d7e04359edd14d
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-06-13 18:18:59 +0900

    가위 바위 보 재커밋

--- Algorithm/taerang/가위 바위 보/내가 푼 코드.js
+++ Algorithm/taerang/가위 바위 보/내가 푼 코드.js
@@ -1,13 +1,15 @@
-let answer = "";
+function solution(rsp) {
+  let answer = "";
 
-for (let i = 0; i < rsp.length; i++) {
-  if (rsp[i] == "2") {
-    answer += "0";
-  } else if (rsp[i] == "5") {
-    answer += "2";
-  } else {
-    answer += "5";
+  for (let i = 0; i < rsp.length; i++) {
+    if (rsp[i] == "2") {
+      answer += "0";
+    } else if (rsp[i] == "5") {
+      answer += "2";
+    } else {
+      answer += "5";
+    }
+    console.log(answer);
   }
-  console.log(answer);
+  return answer;
 }
-return answer;
