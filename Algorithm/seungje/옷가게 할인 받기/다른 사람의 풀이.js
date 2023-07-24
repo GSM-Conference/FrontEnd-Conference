e37ebc11512345fc09a2679676d3d9053c1e1cea
Lee-Seungje <moondgod@gmail.com>
2023-07-24 19:04:44 +0900

    옷가게 할인 받기

---
+++ Algorithm/seungje/옷가게 할인 받기/다른 사람의 풀이.js
@@ -0,0 +1,12 @@
+const discounts = [
+  [500000, 20],
+  [300000, 10],
+  [100000, 5],
+];
+
+const solution = (price) => {
+  for (const discount of discounts)
+    if (price >= discount[0])
+      return Math.floor(price - (price * discount[1]) / 100);
+  return price;
+};
